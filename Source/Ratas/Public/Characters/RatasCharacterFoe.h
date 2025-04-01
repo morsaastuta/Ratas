f6a63075ab1acfbe98fb89cd252b9d848f6548d3
Morsa <morsaastuta@gmail.com>
2025-04-01 19:41:37 +0200

    feat: create foe

---
+++ Source/Ratas/Public/Characters/RatasCharacterFoe.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RatasCharacter.h"
+#include "RatasCharacterFoe.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RATAS_API ARatasCharacterFoe : public ARatasCharacter
+{
+	GENERATED_BODY()
+	
+};
